a985f0c61bfe3ac790d85944dde03bb9b0fb701b
李楠 <1597711363@qq.com>
2020-12-30 15:07:15 +0800

    .

--- src/pages/p1/P1.jsx
+++ src/pages/p1/P1.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import './P1.less'
+import './P1.css'
+import a from './签到.svg'
 
 export default class App extends Component {
 	aa = () => {};
 	componentDidMount() {
 		console.log(require('react-router-dom'));
 		console.log(localStorage.getItem('555555'));
+		// console.log(a)
 	 }
 	render(h) {
 		// console.log(this)
 		return (
 			<div className="p1">
-                <h1>p111111111111111111</h1>
+                <h1>p11111111111111111166</h1>
+				<img src={a} alt=""/>
 			</div>)
 	}
 }
